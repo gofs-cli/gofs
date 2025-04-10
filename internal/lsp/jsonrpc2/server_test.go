4806b4153ae84176a844b46a1d6fe1d4d5ef082e
Mark Cooper <cooperm1700@hotmail.co.uk>
2025-04-10 17:26:24 +0100

    Uncomment tests

--- internal/lsp/jsonrpc2/server_test.go
+++ internal/lsp/jsonrpc2/server_test.go
@@ -1,8 +1,12 @@
 package jsonrpc2
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"io"
 	"sync"
+	"testing"
 	"time"
 
 	"github.com/gofs-cli/gofs/internal/lsp/protocol"
@@ -62,262 +66,262 @@ func testServer(c *Conn) *Server {
 	return s
 }
 
-// func TestListenAndServe(t *testing.T) {
-// 	t.Parallel()
+func TestListenAndServe(t *testing.T) {
+	t.Parallel()
 
-// 	t.Run("server starts, shuts downs, and calls initializer", func(t *testing.T) {
-// 		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
-// 		reader := newTestReader([]protocol.Request{
-// 			{
-// 				Version: "2.0",
-// 				Id:      1,
-// 				Method:  protocol.RequestInitialize,
-// 				Params:  &initParams,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      2,
-// 				Method:  protocol.NotificationInitialized,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      3,
-// 				Method:  protocol.RequestShutdown,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      4,
-// 				Method:  protocol.NotificationExit,
-// 				Params:  nil,
-// 			},
-// 		})
-// 		writer := new(bytes.Buffer)
-// 		conn := NewConn(reader, writer)
-// 		rp := ""
-// 		s, err := NewServer(conn, func(rootPath string) error {
-// 			rp = rootPath
-// 			return nil
-// 		}, protocol.ServerCapabilities{})
-// 		s.HandleLifecycle("initialize", Initialize(s))
-// 		s.HandleLifecycle("initialized", Initialized(s))
-// 		s.HandleLifecycle("shutdown", Shutdown(s))
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 		err = s.ListenAndServe()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 		if rp != "/foo/bar" {
-// 			t.Errorf("expected root path /foo/bar, got %v", rp)
-// 		}
-// 	})
+	t.Run("server starts, shuts downs, and calls initializer", func(t *testing.T) {
+		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
+		reader := newTestReader([]protocol.Request{
+			{
+				Version: "2.0",
+				Id:      1,
+				Method:  protocol.RequestInitialize,
+				Params:  &initParams,
+			},
+			{
+				Version: "2.0",
+				Id:      2,
+				Method:  protocol.NotificationInitialized,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      3,
+				Method:  protocol.RequestShutdown,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      4,
+				Method:  protocol.NotificationExit,
+				Params:  nil,
+			},
+		})
+		writer := new(bytes.Buffer)
+		conn := NewConn(reader, writer)
+		rp := ""
+		s, err := NewServer(conn, func(rootPath string) error {
+			rp = rootPath
+			return nil
+		}, protocol.ServerCapabilities{})
+		s.HandleLifecycle("initialize", Initialize(s))
+		s.HandleLifecycle("initialized", Initialized(s))
+		s.HandleLifecycle("shutdown", Shutdown(s))
+		if err != nil {
+			t.Error(err)
+		}
+		err = s.ListenAndServe()
+		if err != nil {
+			t.Error(err)
+		}
+		if rp != "/foo/bar" {
+			t.Errorf("expected root path /foo/bar, got %v", rp)
+		}
+	})
 
-// 	t.Run("server calls handler", func(t *testing.T) {
-// 		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
-// 		reader := newTestReader([]protocol.Request{
-// 			{
-// 				Version: "2.0",
-// 				Id:      1,
-// 				Method:  protocol.RequestInitialize,
-// 				Params:  &initParams,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      2,
-// 				Method:  protocol.NotificationInitialized,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      3,
-// 				Method:  "foo",
-// 				Params:  nil,
-// 			},
-// 		})
-// 		writer := new(bytes.Buffer)
-// 		conn := NewConn(reader, writer)
-// 		s := testServer(conn)
-// 		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, r protocol.Request) {
-// 			rq <- protocol.NewResponse(r.Id, json.RawMessage(`{"foo": "bar"}`))
-// 		})
-// 		go func() {
-// 			// give the writer time to write the response
-// 			time.Sleep(100 * time.Millisecond)
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      4,
-// 				Method:  protocol.RequestShutdown,
-// 				Params:  nil,
-// 			})
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      5,
-// 				Method:  protocol.NotificationExit,
-// 				Params:  nil,
-// 			})
-// 		}()
-// 		err := s.ListenAndServe()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
+	t.Run("server calls handler", func(t *testing.T) {
+		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
+		reader := newTestReader([]protocol.Request{
+			{
+				Version: "2.0",
+				Id:      1,
+				Method:  protocol.RequestInitialize,
+				Params:  &initParams,
+			},
+			{
+				Version: "2.0",
+				Id:      2,
+				Method:  protocol.NotificationInitialized,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      3,
+				Method:  "foo",
+				Params:  nil,
+			},
+		})
+		writer := new(bytes.Buffer)
+		conn := NewConn(reader, writer)
+		s := testServer(conn)
+		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			rq <- protocol.NewResponse(id, json.RawMessage(`{"foo": "bar"}`))
+		})
+		go func() {
+			// give the writer time to write the response
+			time.Sleep(100 * time.Millisecond)
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      4,
+				Method:  protocol.RequestShutdown,
+				Params:  nil,
+			})
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      5,
+				Method:  protocol.NotificationExit,
+				Params:  nil,
+			})
+		}()
+		err := s.ListenAndServe()
+		if err != nil {
+			t.Error(err)
+		}
 
-// 		// init response
-// 		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
-// 		// request response
-// 		expected += "Content-Length: 60\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"foo":"bar"},"error":null}`
-// 		// shutdown response
-// 		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":null,"error":null}`
-// 		if writer.String() != expected {
-// 			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
-// 		}
-// 	})
+		// init response
+		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
+		// request response
+		expected += "Content-Length: 60\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"foo":"bar"},"error":null}`
+		// shutdown response
+		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":null,"error":null}`
+		if writer.String() != expected {
+			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
+		}
+	})
 
-// 	t.Run("server responds to requests in any order", func(t *testing.T) {
-// 		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
-// 		reader := newTestReader([]protocol.Request{
-// 			{
-// 				Version: "2.0",
-// 				Id:      1,
-// 				Method:  protocol.RequestInitialize,
-// 				Params:  &initParams,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      2,
-// 				Method:  protocol.NotificationInitialized,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      3,
-// 				Method:  "foo", // foo sent first
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      4,
-// 				Method:  "bar", // bar sent second
-// 				Params:  nil,
-// 			},
-// 		})
-// 		writer := new(bytes.Buffer)
-// 		conn := NewConn(reader, writer)
-// 		s := testServer(conn)
-// 		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, r protocol.Request) {
-// 			time.Sleep(100 * time.Millisecond)
-// 			rq <- protocol.NewResponse(r.Id, json.RawMessage(`{"foo": "response"}`))
-// 		})
-// 		s.HandleRequest("bar", func(ctx context.Context, rq chan protocol.Response, r protocol.Request) {
-// 			rq <- protocol.NewResponse(r.Id, json.RawMessage(`{"bar": "response"}`))
-// 		})
-// 		go func() {
-// 			// give the handlers time to respond
-// 			time.Sleep(200 * time.Millisecond)
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      5,
-// 				Method:  protocol.RequestShutdown,
-// 				Params:  nil,
-// 			})
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      6,
-// 				Method:  protocol.NotificationExit,
-// 				Params:  nil,
-// 			})
-// 		}()
-// 		err := s.ListenAndServe()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
+	t.Run("server responds to requests in any order", func(t *testing.T) {
+		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
+		reader := newTestReader([]protocol.Request{
+			{
+				Version: "2.0",
+				Id:      1,
+				Method:  protocol.RequestInitialize,
+				Params:  &initParams,
+			},
+			{
+				Version: "2.0",
+				Id:      2,
+				Method:  protocol.NotificationInitialized,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      3,
+				Method:  "foo", // foo sent first
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      4,
+				Method:  "bar", // bar sent second
+				Params:  nil,
+			},
+		})
+		writer := new(bytes.Buffer)
+		conn := NewConn(reader, writer)
+		s := testServer(conn)
+		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			time.Sleep(100 * time.Millisecond)
+			rq <- protocol.NewResponse(id, json.RawMessage(`{"foo": "response"}`))
+		})
+		s.HandleRequest("bar", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			rq <- protocol.NewResponse(id, json.RawMessage(`{"bar": "response"}`))
+		})
+		go func() {
+			// give the handlers time to respond
+			time.Sleep(200 * time.Millisecond)
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      5,
+				Method:  protocol.RequestShutdown,
+				Params:  nil,
+			})
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      6,
+				Method:  protocol.NotificationExit,
+				Params:  nil,
+			})
+		}()
+		err := s.ListenAndServe()
+		if err != nil {
+			t.Error(err)
+		}
 
-// 		// init response
-// 		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
-// 		// bar first
-// 		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":{"bar":"response"},"error":null}`
-// 		// foo second
-// 		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"foo":"response"},"error":null}`
-// 		// shutdown response
-// 		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":5,"result":null,"error":null}`
+		// init response
+		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
+		// bar first
+		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":{"bar":"response"},"error":null}`
+		// foo second
+		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"foo":"response"},"error":null}`
+		// shutdown response
+		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":5,"result":null,"error":null}`
 
-// 		if writer.String() != expected {
-// 			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
-// 		}
-// 	})
+		if writer.String() != expected {
+			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
+		}
+	})
 
-// 	t.Run("server cancels request", func(t *testing.T) {
-// 		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
-// 		cancelParams := json.RawMessage(`{"id": 3}`)
-// 		reader := newTestReader([]protocol.Request{
-// 			{
-// 				Version: "2.0",
-// 				Id:      1,
-// 				Method:  protocol.RequestInitialize,
-// 				Params:  &initParams,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      2,
-// 				Method:  protocol.NotificationInitialized,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      3,
-// 				Method:  "foo",
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      4,
-// 				Method:  "$/cancelRequest",
-// 				Params:  &cancelParams,
-// 			},
-// 		})
-// 		writer := new(bytes.Buffer)
-// 		conn := NewConn(reader, writer)
-// 		s := testServer(conn)
-// 		cancelled := false
-// 		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, r protocol.Request) {
-// 			// give the cancel request time to be processed
-// 			time.Sleep(100 * time.Millisecond)
-// 			select {
-// 			case <-ctx.Done():
-// 				cancelled = true
-// 			default:
-// 				rq <- protocol.NewResponse(r.Id, json.RawMessage(`{"foo": "response"}`))
-// 			}
-// 		})
-// 		go func() {
-// 			// give the handlers time to respond
-// 			time.Sleep(200 * time.Millisecond)
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      5,
-// 				Method:  protocol.RequestShutdown,
-// 				Params:  nil,
-// 			})
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      6,
-// 				Method:  protocol.NotificationExit,
-// 				Params:  nil,
-// 			})
-// 		}()
-// 		err := s.ListenAndServe()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
+	t.Run("server cancels request", func(t *testing.T) {
+		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
+		cancelParams := json.RawMessage(`{"id": 3}`)
+		reader := newTestReader([]protocol.Request{
+			{
+				Version: "2.0",
+				Id:      1,
+				Method:  protocol.RequestInitialize,
+				Params:  &initParams,
+			},
+			{
+				Version: "2.0",
+				Id:      2,
+				Method:  protocol.NotificationInitialized,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      3,
+				Method:  "foo",
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      4,
+				Method:  "$/cancelRequest",
+				Params:  &cancelParams,
+			},
+		})
+		writer := new(bytes.Buffer)
+		conn := NewConn(reader, writer)
+		s := testServer(conn)
+		cancelled := false
+		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			// give the cancel request time to be processed
+			time.Sleep(100 * time.Millisecond)
+			select {
+			case <-ctx.Done():
+				cancelled = true
+			default:
+				rq <- protocol.NewResponse(id, json.RawMessage(`{"foo": "response"}`))
+			}
+		})
+		go func() {
+			// give the handlers time to respond
+			time.Sleep(200 * time.Millisecond)
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      5,
+				Method:  protocol.RequestShutdown,
+				Params:  nil,
+			})
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      6,
+				Method:  protocol.NotificationExit,
+				Params:  nil,
+			})
+		}()
+		err := s.ListenAndServe()
+		if err != nil {
+			t.Error(err)
+		}
 
-// 		if !cancelled {
-// 			t.Errorf("expected request to be cancelled but want not")
-// 		}
-// 		isRunning, _ := s.isRunning(3)
-// 		if isRunning {
-// 			t.Errorf("expected request to be cancelled but was not")
-// 		}
-// 	})
-// }
+		if !cancelled {
+			t.Errorf("expected request to be cancelled but want not")
+		}
+		isRunning, _ := s.isRunning(3)
+		if isRunning {
+			t.Errorf("expected request to be cancelled but was not")
+		}
+	})
+}
--- internal/lsp/repo/repo_test.go
+++ internal/lsp/repo/repo_test.go
@@ -1,8 +1,12 @@
 package repo
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"io"
 	"sync"
+	"testing"
 	"time"
 
 	"github.com/gofs-cli/gofs/internal/lsp/jsonrpc2"
@@ -63,175 +67,175 @@ func testServer(c *jsonrpc2.Conn) *jsonrpc2.Server {
 	return s
 }
 
-// func TestListenAndServe(t *testing.T) {
-// 	t.Parallel()
+func TestListenAndServe(t *testing.T) {
+	t.Parallel()
 
-// 	t.Run("handler blocks when it requires a file and didOpen is running", func(t *testing.T) {
-// 		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
-// 		reader := newTestReader([]protocol.Request{
-// 			{
-// 				Version: "2.0",
-// 				Id:      1,
-// 				Method:  protocol.RequestInitialize,
-// 				Params:  &initParams,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      2,
-// 				Method:  protocol.NotificationInitialized,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      3,
-// 				Method:  "didOpen",
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      4,
-// 				Method:  "foo", // foo should wait for didOpen to finish
-// 				Params:  nil,
-// 			},
-// 		})
-// 		writer := new(bytes.Buffer)
-// 		conn := jsonrpc2.NewConn(reader, writer)
-// 		s := testServer(conn)
-// 		r := NewRepo()
-// 		s.HandleRequest("didOpen", func(ctx context.Context, rq chan protocol.Response, rs protocol.Request) {
-// 			r.OpenTemplFile(DidOpenRequest{
-// 				TextDocument: protocol.TextDocument{
-// 					Path: "/foo/bar/templ.templ",
-// 					Text: `package test
+	t.Run("handler blocks when it requires a file and didOpen is running", func(t *testing.T) {
+		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
+		reader := newTestReader([]protocol.Request{
+			{
+				Version: "2.0",
+				Id:      1,
+				Method:  protocol.RequestInitialize,
+				Params:  &initParams,
+			},
+			{
+				Version: "2.0",
+				Id:      2,
+				Method:  protocol.NotificationInitialized,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      3,
+				Method:  "didOpen",
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      4,
+				Method:  "foo", // foo should wait for didOpen to finish
+				Params:  nil,
+			},
+		})
+		writer := new(bytes.Buffer)
+		conn := jsonrpc2.NewConn(reader, writer)
+		s := testServer(conn)
+		r := NewRepo()
+		s.HandleRequest("didOpen", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			r.OpenTemplFile(protocol.DidOpenRequest{
+				TextDocument: protocol.TextDocument{
+					Path: "/foo/bar/templ.templ",
+					Text: `package test
 
-// templ Test() {}
-// `,
-// 				},
-// 			})
-// 			rq <- protocol.NewResponse(rs.Id, json.RawMessage(`{"didOpen": "response"}`))
-// 		})
-// 		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, rs protocol.Request) {
-// 			t := r.GetTemplFile("/foo/bar/templ.templ")
-// 			if t == nil {
-// 				rq <- protocol.NewResponse(rs.Id, json.RawMessage(`{"foo": "fail"}`))
-// 			} else {
-// 				rq <- protocol.NewResponse(rs.Id, json.RawMessage(`{"foo": "response"}`))
-// 			}
-// 		})
-// 		go func() {
-// 			// give the handlers time to respond
-// 			time.Sleep(200 * time.Millisecond)
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      5,
-// 				Method:  protocol.RequestShutdown,
-// 				Params:  nil,
-// 			})
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      6,
-// 				Method:  protocol.NotificationExit,
-// 				Params:  nil,
-// 			})
-// 		}()
-// 		err := s.ListenAndServe()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
+templ Test() {}
+`,
+				},
+			})
+			rq <- protocol.NewResponse(id, json.RawMessage(`{"didOpen": "response"}`))
+		})
+		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			t := r.GetTemplFile("/foo/bar/templ.templ")
+			if t == nil {
+				rq <- protocol.NewResponse(id, json.RawMessage(`{"foo": "fail"}`))
+			} else {
+				rq <- protocol.NewResponse(id, json.RawMessage(`{"foo": "response"}`))
+			}
+		})
+		go func() {
+			// give the handlers time to respond
+			time.Sleep(200 * time.Millisecond)
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      5,
+				Method:  protocol.RequestShutdown,
+				Params:  nil,
+			})
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      6,
+				Method:  protocol.NotificationExit,
+				Params:  nil,
+			})
+		}()
+		err := s.ListenAndServe()
+		if err != nil {
+			t.Error(err)
+		}
 
-// 		// init response
-// 		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
-// 		// didOpen first
-// 		expected += "Content-Length: 69\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"didOpen":"response"},"error":null}`
-// 		// foo second, should wait for didOpen to finish
-// 		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":{"foo":"response"},"error":null}`
-// 		// shutdown response
-// 		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":5,"result":null,"error":null}`
+		// init response
+		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
+		// didOpen first
+		expected += "Content-Length: 69\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"didOpen":"response"},"error":null}`
+		// foo second, should wait for didOpen to finish
+		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":{"foo":"response"},"error":null}`
+		// shutdown response
+		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":5,"result":null,"error":null}`
 
-// 		if writer.String() != expected {
-// 			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
-// 		}
-// 	})
+		if writer.String() != expected {
+			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
+		}
+	})
 
-// 	t.Run("handler does not blocks when it does not requires a file", func(t *testing.T) {
-// 		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
-// 		reader := newTestReader([]protocol.Request{
-// 			{
-// 				Version: "2.0",
-// 				Id:      1,
-// 				Method:  protocol.RequestInitialize,
-// 				Params:  &initParams,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      2,
-// 				Method:  protocol.NotificationInitialized,
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      3,
-// 				Method:  "didOpen",
-// 				Params:  nil,
-// 			},
-// 			{
-// 				Version: "2.0",
-// 				Id:      4,
-// 				Method:  "foo", // foo should wait for didOpen to finish
-// 				Params:  nil,
-// 			},
-// 		})
-// 		writer := new(bytes.Buffer)
-// 		conn := jsonrpc2.NewConn(reader, writer)
-// 		s := testServer(conn)
-// 		r := NewRepo()
-// 		s.HandleRequest("didOpen", func(ctx context.Context, rq chan protocol.Response, rs protocol.Request) {
-// 			r.OpenTemplFile(DidOpenRequest{
-// 				TextDocument: protocol.TextDocument{
-// 					Path: "/foo/bar/templ.templ",
-// 					Text: `package test
+	t.Run("handler does not blocks when it does not requires a file", func(t *testing.T) {
+		initParams := json.RawMessage(`{"rootPath": "/foo/bar"}`)
+		reader := newTestReader([]protocol.Request{
+			{
+				Version: "2.0",
+				Id:      1,
+				Method:  protocol.RequestInitialize,
+				Params:  &initParams,
+			},
+			{
+				Version: "2.0",
+				Id:      2,
+				Method:  protocol.NotificationInitialized,
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      3,
+				Method:  "didOpen",
+				Params:  nil,
+			},
+			{
+				Version: "2.0",
+				Id:      4,
+				Method:  "foo", // foo should wait for didOpen to finish
+				Params:  nil,
+			},
+		})
+		writer := new(bytes.Buffer)
+		conn := jsonrpc2.NewConn(reader, writer)
+		s := testServer(conn)
+		r := NewRepo()
+		s.HandleRequest("didOpen", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			r.OpenTemplFile(protocol.DidOpenRequest{
+				TextDocument: protocol.TextDocument{
+					Path: "/foo/bar/templ.templ",
+					Text: `package test
 
-// templ Test() {}
-// `,
-// 				},
-// 			})
-// 			rq <- protocol.NewResponse(rs.Id, json.RawMessage(`{"didOpen": "response"}`))
-// 		})
-// 		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, rs protocol.Request) {
-// 			rq <- protocol.NewResponse(rs.Id, json.RawMessage(`{"foo": "response"}`))
-// 		})
-// 		go func() {
-// 			// give the handlers time to respond
-// 			time.Sleep(200 * time.Millisecond)
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      5,
-// 				Method:  protocol.RequestShutdown,
-// 				Params:  nil,
-// 			})
-// 			reader.addReq(protocol.Request{
-// 				Version: "2.0",
-// 				Id:      6,
-// 				Method:  protocol.NotificationExit,
-// 				Params:  nil,
-// 			})
-// 		}()
-// 		err := s.ListenAndServe()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
+templ Test() {}
+`,
+				},
+			})
+			rq <- protocol.NewResponse(id, json.RawMessage(`{"didOpen": "response"}`))
+		})
+		s.HandleRequest("foo", func(ctx context.Context, rq chan protocol.Response, params any, id int) {
+			rq <- protocol.NewResponse(id, json.RawMessage(`{"foo": "response"}`))
+		})
+		go func() {
+			// give the handlers time to respond
+			time.Sleep(200 * time.Millisecond)
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      5,
+				Method:  protocol.RequestShutdown,
+				Params:  nil,
+			})
+			reader.addReq(protocol.Request{
+				Version: "2.0",
+				Id:      6,
+				Method:  protocol.NotificationExit,
+				Params:  nil,
+			})
+		}()
+		err := s.ListenAndServe()
+		if err != nil {
+			t.Error(err)
+		}
 
-// 		// init response
-// 		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
-// 		// foo should return first
-// 		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":{"foo":"response"},"error":null}`
-// 		// didOpen second
-// 		expected += "Content-Length: 69\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"didOpen":"response"},"error":null}`
-// 		// shutdown response
-// 		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":5,"result":null,"error":null}`
+		// init response
+		expected := "Content-Length: 206\r\n\r\n" + `{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":0,"hoverProvider":false,"diagnosticProvider":{"identifier":"","interFileDependencies":false,"workspaceDiagnostics":false}}},"error":null}`
+		// foo should return first
+		expected += "Content-Length: 65\r\n\r\n" + `{"jsonrpc":"2.0","id":4,"result":{"foo":"response"},"error":null}`
+		// didOpen second
+		expected += "Content-Length: 69\r\n\r\n" + `{"jsonrpc":"2.0","id":3,"result":{"didOpen":"response"},"error":null}`
+		// shutdown response
+		expected += "Content-Length: 51\r\n\r\n" + `{"jsonrpc":"2.0","id":5,"result":null,"error":null}`
 
-// 		if writer.String() != expected {
-// 			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
-// 		}
-// 	})
-// }
+		if writer.String() != expected {
+			t.Errorf("expected:\n%v\ngot:\n%v", expected, writer.String())
+		}
+	})
+}
