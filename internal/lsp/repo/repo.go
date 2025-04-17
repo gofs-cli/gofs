bff28382700c20e50c2f07fa597ff7c468b9cd65
Ziqiongg <ziqili33@gmail.com>
2025-04-17 15:20:41 +0100

    clean

--- internal/lsp/repo/repo.go
+++ internal/lsp/repo/repo.go
@@ -118,9 +118,6 @@ func (r *Repo) OpenTemplFile(req DidOpenRequest) {
 		return
 	}
 
-	log.Println("All routes in OpenTemplFile")
-	log.Println(r.rt.Routes())
-
 	uriRouteIndex := make([]int, len(uris))
 	for i := range uris {
 		uriRouteIndex[i] = r.rt.RouteIndex(uris[i])
