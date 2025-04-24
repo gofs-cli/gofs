15f845aded5805a6cab1402a56b9b50749b589de
kavindapelpola <kavinda.pelpola@gmail.com>
2025-04-24 10:39:01 +0100

    lsp debug

--- internal/cmd/lsp.go
+++ internal/cmd/lsp.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"flag"
+	"os"
 
 	"github.com/gofs-cli/gofs/internal/lsp"
 )
 
-const lspUsage = `usage: gofs lsp
+const lspUsage = `usage: gofs lsp [-debug] [-stdio]
 
 Experimental: "lsp" starts the gofs language server.
 `
@@ -15,7 +16,7 @@ func init() {
 	Gofs.AddCmd(Command{
 		Name:  "lsp",
 		Short: "Experimental: start the gofs language server",
-		Long:  initUsage,
+		Long:  lspUsage,
 		Cmd:   cmdLsp,
 	})
 }
@@ -23,7 +24,8 @@ func init() {
 func cmdLsp() {
 	fs := flag.NewFlagSet("lsp", flag.ExitOnError)
 	debug := fs.Bool("debug", false, "enable debug logging")
-	//	_ = fs.Parse(flag.Args()[1:]) // skip "lsp" itself
+	_ = fs.Bool("stdio", true, "use stdio")
+	_ = fs.Parse(os.Args[2:]) // skip program name and command
 
 	lsp.Start(*debug)
 }
