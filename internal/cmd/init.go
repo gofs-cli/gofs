1954784da5bc21822d40f3d93c9bbdf2483f1069
Steven Lee <steven.lee@kynrai.com>
2025-05-21 10:51:39 +0100

    feat: allow template selection for init (#24)

--- internal/cmd/init.go
+++ internal/cmd/init.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"embed"
+	"flag"
 	"fmt"
 	"os"
 
-	folder "github.com/gofs-cli/template"
+	azureTemplate "github.com/gofs-cli/azure-app-template"
+	defaultTemplate "github.com/gofs-cli/template"
 
 	"github.com/gofs-cli/gofs/internal/gen"
 )
@@ -21,9 +24,20 @@ If no directory is specified, the current directory is used.
 
 The module name should be a go module name, e.g. "github.com/user/module".
 
+flags:
+  -template
+    Name of the template to use for the project. By default this will ues the basic bare bones template.
+
+    Available names:
+      - azure
+          This template creates an app suitable for deployment to azure apps and expects azure auth tokens from Entra ID
+
+
 Example:
-  gofs init mymodule /path/to/dir
   gofs init mymodule
+  gofs init mymodule /path/to/dir
+  gofs init -template=azure mymodule
+  gofs init -template=azure mymodule /path/to/dir
 
 `
 
@@ -37,34 +51,51 @@ func init() {
 }
 
 func cmdInit() {
-	args := os.Args[2:] // skip program name and command
+	var template string
+	fs := flag.NewFlagSet("init", flag.ContinueOnError)
+	fs.StringVar(&template, "template", "default", "the template to use for the generated project")
 
+	args := os.Args[2:] // skip program name and command name
+	var err error
+	err = fs.Parse(args)
+	if err != nil {
+		fmt.Println("init: erorr parsing ", err)
+		return
+	}
+
+	fmt.Println("template is: ", template)
 	moduleName := ""
 	dir := ""
-	var err error
 
-	switch {
-	case len(args) == 0:
+	switch fs.NArg() {
+	case 0:
 		fmt.Println("init: missing module name")
 		fmt.Print(initUsage)
 		return
-	case len(args) == 1:
-		moduleName = args[0]
+	case 1:
+		moduleName = fs.Arg(0)
 		dir, err = os.Getwd()
 		if err != nil {
 			fmt.Println("init: ", err)
 			return
 		}
-	case len(args) == 2:
-		moduleName = args[0]
-		dir = args[1]
+	case 2:
+		moduleName = fs.Arg(0)
+		dir = fs.Arg(1)
 	default:
 		fmt.Println("init: too many arguments")
 		fmt.Print(initUsage)
 		return
 	}
 
-	parser, err := gen.NewParser(dir, defaultModuleName, moduleName, folder.Folder)
+	var selectedTemplate embed.FS
+	switch template {
+	case "azure":
+		selectedTemplate = azureTemplate.Folder
+	default:
+		selectedTemplate = defaultTemplate.Folder
+	}
+	parser, err := gen.NewParser(dir, defaultModuleName, moduleName, selectedTemplate)
 	if err != nil {
 		fmt.Println("init: ", err)
 		return
